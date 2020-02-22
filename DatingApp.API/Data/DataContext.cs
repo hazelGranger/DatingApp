6cdadf09b3c2167ede1f67144b0651100798025b
hazelGranger <wangzhen_blcu@163.com>
2020-02-23 12:23:06 +1300

    init

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Model.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        // tell dbcontext about our entities
+        public DbSet<Value> Values { get; set; }
+    }
+}
